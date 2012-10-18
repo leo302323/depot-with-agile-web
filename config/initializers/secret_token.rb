c78f1c22327514b856f7e10cc8f73d8a563a64d7
saasbook <saasbook@saasbook.(none)>
2012-10-18 00:11:17 -0700

    depot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '50e08363b9db7697a4db000293934b46c2e305753858e98953b2280f088db07af89b3ffcf7fe70a44aca463bbab15b75269e0a88dc3fac0d926c2feea09034e3'
