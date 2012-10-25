10cf7a718236c99f1b9898ab9414d1da1036715e
saasbook <saasbook@saasbook.(none)>
2012-10-25 00:33:43 -0700

    final

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,5 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :cart_id, :product_id,:quantity
+  attr_accessible :cart_id, :product_id,:quantity,:order_id
   belongs_to :product
   belongs_to :cart
   
